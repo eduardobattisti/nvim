5c57fde03009008be8d49a1aafd5b0e7040e562e
eduardobattisti <eduardobattisti10@gmail.com>
2024-07-14 00:21:35 -0300

    refactor: remove notify test message

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -6,11 +6,5 @@ return {
     notify.setup(opts)
 
     vim.notify = notify
-    vim.notify('This is an info message 1', 'info')
-    vim.notify('This is an info message 2', 'info')
-    vim.notify('This is an info message 3', 'info')
-    vim.notify('This is an info message 4', 'info')
-    vim.notify('This is an info message 5', 'info')
-    vim.notify('This is an info message 6', 'info')
   end,
 }
