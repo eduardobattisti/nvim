d5a2d02d271a6bef1152b188a7aa4e7148e45721
eduardobattisti <eduardobattisti10@gmail.com>
2024-07-14 00:21:16 -0300

    refactor: disable close_if_last_window

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
     { '<Leader>e', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' } },
   },
   opts = {
-    close_if_last_window = true,
+    close_if_last_window = false,
     enable_git_status = true,
     enable_diagnostics = true,
     sources = { 'filesystem', 'buffers', 'git_status' },
