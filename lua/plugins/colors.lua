f31c6f32fc6d4fd83dc57e648455e66252076e0c
eduardobattisti <eduardobattisti10@gmail.com>
2024-07-15 12:45:41 -0300

    feat: uses rose-pine moon theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,19 +1,13 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'sainnhe/gruvbox-material',
-  name = 'gruvbox-material',
+return {
+  'rose-pine/neovim',
+  name = 'rose-pine',
   priority = 1000, -- Make sure to load this before all the other start plugins.
+  lazy = false,
+  opts = {
+    variant = 'moon',
+  },
   init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'gruvbox-material'
-    vim.opt.termguicolors = true
-
-    -- You can configure highlights by doing something like:
+    vim.cmd.colorscheme 'rose-pine'
     vim.cmd.hi 'Comment gui=none'
   end,
 }
