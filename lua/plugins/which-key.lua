38eb439cc268b22bbacbff303d4ac9814c65f42e
eduardobattisti <eduardobattisti10@gmail.com>
2024-07-15 00:04:56 -0300

    refactor: update group keymaps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,24 @@
 return { -- Useful plugin to show you pending keybinds. 'folke/which-key.nvim',
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
+  opts = {
+    win = {
+      title = false,
+      border = 'single',
+    },
+  },
+  config = function(_, opts) -- This is the function that runs, AFTER loading
+    require('which-key').setup(opts)
 
     -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]ession', _ = 'which_key_ignore' },
-      ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+    require('which-key').add {
+      { '<leader>l', group = '[L]sp', icon = '' },
+      { '<leader>d', group = '[D]ebug', icon = '' },
+      { '<leader>S', group = '[S]ession', icon = '' },
+      { '<leader>f', group = '[F]ind', icon = '󰭎' },
+      { '<leader>t', group = '[T]erminal', icon = ' ' },
+      { '<leader>g', group = '[G]it', icon = '' },
+      { '<leader>c', group = '[C]onfig', icon = '' },
     }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
   end,
 }
