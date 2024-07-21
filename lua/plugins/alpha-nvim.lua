389176a0458bfd9cb80529b81b7736aad808ab4e
eduardobattisti <eduardobattisti10@gmail.com>
2024-07-21 11:24:21 -0300

    reactor: simplify alpha nvim start

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -8,54 +8,16 @@ return {
   config = function()
     -- Import alpha and dashboard
     local alpha = require 'alpha'
-    local dashboard = require 'alpha.themes.dashboard'
-
-    -- Define custom icons
-    local icons = {
-      ui = {
-        file = '',
-        files = '',
-        open_folder = '',
-        session = '󰙅',
-        close = '󰈆',
-        git = '',
-      },
-    }
+    local dashboard = require 'alpha.themes.startify'
 
     local function whitespace_only(str)
       return str:match '^%s*$' ~= nil
     end
 
-    local function pad(n)
-      local padding = { type = 'padding', val = n }
-
-      if n == 'auto' then
-        local height = vim.api.nvim_win_get_height(0)
-        padding.val = math.floor(height / 4)
-
-        return padding
-      end
-      return padding
-    end
-
-    local function center_header(header)
-      local lines = {}
-      for line in string.gmatch(header, '[^\n]+') do
-        table.insert(lines, line)
-      end
-
-      local width = vim.api.nvim_win_get_width(0) -- Get the width of the current window
-      local padding = math.floor((width - #lines[1]) / 0.8) -- Calculate padding based on the first line length
-      for i, line in ipairs(lines) do
-        lines[i] = string.rep(' ', padding) .. line
-      end
-
-      return lines
-    end
-
-    local function format_git_header()
+    local function git_header()
       local git_dashboard_raw = require('git-dashboard-nvim').setup {}
       local git_dashboard = {}
+
       for _, line in ipairs(git_dashboard_raw) do
         if not whitespace_only(line) then
           table.insert(git_dashboard, line)
@@ -66,94 +28,27 @@ return {
       local git_branch = git_dashboard[#git_dashboard]
 
       if git_repo == nil and git_branch == nil then
-        return {
-          type = 'text',
-          val = '',
-          opts = { position = 'center' },
-        }
+        return ''
       end
 
-      local git_branch_section = {
-        type = 'text',
-        val = ' ' .. git_repo .. ':' .. git_branch,
-        opts = { position = 'center' },
-      }
-
-      return git_branch_section
+      return ' ' .. git_repo .. ':' .. git_branch
     end
 
-    -- Define custom header with ASCII art or any custom message
-    local ascii_header = [[
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠔⣰⣿⣟⡽⢋⡴⣿⠏⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠹⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⡿⣡⠏⣴⡟⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢹⣿⣿⣿⣿⡿⠏⠁⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⣠⢾⣿⣿⡛⣰⡏⣼⣿⡇⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⣿⡿⢛⣻⡅⠀⡆⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣠⡟⣸⣿⣿⢠⣿⠃⣿⣿⠃⣼⢿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⡇⢿⡇⣿⣿⠃⢠⡟⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢠⣿⢃⣿⣿⠇⣾⡟⠀⣿⢇⣴⡄⢸⣿⣿⣿⡟⣿⣧⠘⣿⣿⡇⣿⢿⢻⣷⠸⣇⢿⣿⠀⠀⣧⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⡼⢹⡿⢸⣿⡿⢸⠟⣵⡿⠈⠈⠿⣷⠈⣿⣿⣿⠆⢿⣷⢀⢻⣿⡆⢹⢼⣷⣿⢀⣿⡜⢿⠦⠀⠩⡄⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣘⡇⣾⡿⢸⣿⡇⣠⡾⢋⣶⣦⠁⢒⡘⣷⡘⣿⣿⡄⣎⠻⡼⣧⡙⣿⣌⠘⣿⣿⢸⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⣰⣿⡇⢿⣿⢸⡿⢰⣿⢡⣿⠉⣯⣄⠀⣷⣿⣿⣌⡻⢧⢹⣷⢆⣉⠠⠄⡀⢢⠹⣿⢸⣿⣿⣿⡿⢱⣦⠄⢠⣶⠀⠀⠀⠀
-⠀⠀⣰⣿⣿⣷⢸⣿⠸⣿⢸⡏⣼⣿⠀⠟⠁⣸⣿⣿⣿⣿⣿⣷⣶⣴⡟⠿⡁⠀⢹⡎⢷⡹⢸⣿⣿⠟⣠⣿⠏⣼⢸⣿⡃⠀⠀⠀
-⠀⠀⣿⣿⣿⣿⡆⣿⡇⣿⢸⣿⡸⣿⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⣷⡀⣸⣿⢸⣇⢸⣿⡏⣸⣿⡏⣸⣿⢸⣿⡇⠀⠀⠀
-⠀⠀⢿⣿⣿⣿⣷⠸⣷⠸⢸⣿⣷⣮⣛⣛⣋⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠈⢠⣠⣿⡿⢸⠏⣾⢋⣴⣿⢋⣴⣿⣿⢸⣿⡏⡶⠀⠀
-⠀⡇⣿⣿⣿⣿⣿⣧⡹⣧⠈⣿⣿⣿⣿⣿⣿⣿⡟⠛⠿⢿⣿⣿⣟⠿⣿⣿⣿⣿⡿⣣⡟⠜⣱⣿⠟⣴⣿⣿⣿⣿⢸⣿⠃⣟⡇⠀
-⢘⠀⢻⣿⣿⣿⣿⣿⣷⡘⢆⠻⣿⣿⣿⣿⣿⡿⣰⣿⣿⡇⢿⣿⣿⣿⣶⣭⣭⣭⣾⡟⣠⣾⠟⣥⣿⣿⣿⣿⣿⡇⣼⡿⠸⣸⣿⡄
-⢈⠀⠸⣿⣿⣿⣿⣿⣿⣿⡆⠀⠉⠻⢿⣿⣿⡇⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⢏⡼⢛⣴⣿⣿⣿⣿⣿⣿⣿⢁⣿⢡⢃⡏⣿⡇
-⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⢰⡍⣛⠷⢭⣙⣛⣛⣸⣿⣿⣿⣿⣿⡿⠡⢊⣴⣿⣿⣿⣿⣿⣿⣿⣿⡏⣸⢇⡎⠘⡇⣿⡇
-⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⠀⢠⣼⣷⣬⠛⣶⣬⣭⠉⡍⠋⠙⢩⣭⡍⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢣⡎⠀⠀⢹⣿⠃
-⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⢣⣾⣿⣿⢿⣿⣇⢹⣻⠿⢛⣩⣭⣝⠻⣿⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠁⠀⠀⠀⣼⡟⠀
-⠀⠀⠀⠀⠀⠀⠀⠛⢿⣿⢃⣿⣿⢿⣿⣿⣿⠉⡴⠉⠂⣾⣿⡿⢿⣷⡄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⢀⠀⠂⠀⠟⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢸⣿⣿⣿⣮⣛⠿⢣⠰⠧⠾⣿⣿⣿⣿⣾⣿⢸⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⢀⠐⠀⡀⠄⠀⠀
-		]]
-
-    local git_branch_section = format_git_header()
-
-    local custom_header = center_header(ascii_header)
-    local header = { type = 'text', val = custom_header }
-
-    local buttons = {
-      type = 'group',
-      val = {
-        dashboard.button('e', icons.ui.file .. '  New file', '<cmd>new<CR>'),
-        dashboard.button('f', icons.ui.files .. '  Find Files', '<cmd>Telescope find_files<cr>'),
-        dashboard.button('o', icons.ui.files .. '  Recent Files', '<cmd>Telescope oldfiles<cr>'),
-        dashboard.button('g', icons.ui.git .. '  Lazy Git', '<cmd>LazyGit<CR>'),
-        dashboard.button('s', icons.ui.session .. '  Show Sessions', "<cmd>lua require('resession').load()<CR>"),
-        dashboard.button('q', icons.ui.close .. '  Quit NVIM', ':qa<CR>'),
-      },
-      opts = {
-        spacing = 1,
-        position = 'center',
-      },
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      git_header(),
     }
 
-    -- Custom footer showing number of plugins loaded
-    local footer = {
-      type = 'text',
-      val = { '⚡' .. require('lazy').stats().loaded .. ' plugins loaded.' },
-      opts = { position = 'center', hl = 'Comment' },
-    }
-
-    -- Custom section with a personal greeting
-    local bottom_section = {
-      type = 'text',
-      val = "Hi eduardobattisti. It's" .. os.date ' %H:%M.' .. ' How are you doing today?',
-      opts = { position = 'center' },
-    }
-
-    -- Setting up the alpha layout
-    alpha.setup {
-      layout = {
-        pad 'auto',
-        header,
-        pad(1),
-        git_branch_section,
-        pad(2),
-        buttons,
-        pad(2),
-        bottom_section,
-        pad(2),
-        footer,
-      },
-    }
+    alpha.setup(dashboard.opts)
   end,
 }
