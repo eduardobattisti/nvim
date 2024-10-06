363f8dedd20c6275da17adec7478957a91d9aced
eduardobattisti <eduardobattisti10@gmail.com>
2024-10-06 10:41:19 -0300

    refactor: rename plugin according to plugin name

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,13 +1,12 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
-    require('nvim-autopairs').setup {}
+    require('nvim-autopairs').setup {
+      check_ts = true,
+    }
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
