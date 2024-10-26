4bfd42a6fab2c85a3a896aea2f6474bec593efd6
eduardobattisti <eduardobattisti10@gmail.com>
2024-10-26 11:13:19 -0300

    refactor: use mapping utils

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -67,9 +67,9 @@ return { -- LSP Configuration & Plugins
         --
         -- This may be unwanted, since they displace some of your code
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-          map('<leader>lth', function()
+          lsp_utils.keymap('<leader>lth', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end, '[T]oggle Inlay [H]ints')
+          end, event.buf, '[T]oggle Inlay [H]ints')
         end
       end,
     })
