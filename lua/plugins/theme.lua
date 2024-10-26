c9e28ee2c4a764758c1c09460f3695f85427f9a2
eduardobattisti <eduardobattisti10@gmail.com>
2024-10-26 12:49:51 -0300

    refactor: define module variable

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,9 +3,9 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require('bamboo').setup {
-      -- optional configuration here
-    }
-    require('bamboo').load()
+    local bamboo = require 'bamboo'
+
+    bamboo.setup {}
+    bamboo.load()
   end,
 }
