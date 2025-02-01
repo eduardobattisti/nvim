dccb3e5b867e281ef6b0e4fc57920f73e8c7c349
Eduardo Battisti <eduadobattisti@pop-os.localdomain>
2025-02-01 17:37:38 -0300

    refactor: remove git header

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -14,26 +14,6 @@ return {
       return str:match '^%s*$' ~= nil
     end
 
-    local function git_header()
-      local git_dashboard_raw = require('git-dashboard-nvim').setup {}
-      local git_dashboard = {}
-
-      for _, line in ipairs(git_dashboard_raw) do
-        if not whitespace_only(line) then
-          table.insert(git_dashboard, line)
-        end
-      end
-
-      local git_repo = git_dashboard[1]
-      local git_branch = git_dashboard[#git_dashboard]
-
-      if git_repo == nil and git_branch == nil then
-        return ''
-      end
-
-      return ' ' .. git_repo .. ':' .. git_branch
-    end
-
     dashboard.section.header.val = {
       [[                                                                       ]],
       [[                                                                     ]],
@@ -46,7 +26,6 @@ return {
       [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
       [[                                                                       ]],
       [[                                                                       ]],
-      git_header(),
     }
 
     alpha.setup(dashboard.opts)
