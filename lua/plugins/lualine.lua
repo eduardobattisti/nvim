95354b9fa8ae46ac869d1dfe58c87da1216fc992
Eduardo Battisti <eduardobattisti10@gmail.com>
2025-03-16 23:19:56 -0300

    refactor: improve lualine with recording status

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,22 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
+    sections = {
+      lualine_a = {
+        function()
+          local reg = vim.fn.reg_recording()
+          -- If a macro is being recorded, show "Recording @<register>"
+          if reg ~= '' then
+            return 'Recording @' .. reg
+          else
+            -- Get the full mode name using nvim_get_mode()
+            local mode = require('lualine.utils.mode').get_mode()
+
+            return mode:upper()
+          end
+        end,
+      },
+    },
     options = {
       icons_enabled = true,
       disabled_filetypes = {
