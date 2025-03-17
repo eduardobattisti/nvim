d78ef93cb8adf9bffa1182d8644540e6e7a01290
Eduardo Battisti <eduardobattisti10@gmail.com>
2025-03-16 22:34:16 -0300

    feat: improve intelephense lsp config

--- lua/config/lsp/servers/intelephense.lua
+++ lua/config/lsp/servers/intelephense.lua
@@ -1,12 +1,26 @@
+root_dir = function(pattern)
+  -- Add wp-config to root-dir
+  local cwd = vim.loop.cwd()
+  local util = require 'lspconfig.util'
+  local root = util.root_pattern('composer.json', '.git', 'wp-config.php')(pattern)
+
+  -- prefer cwd if root is a descendant
+  return util.path.is_descendant(cwd, root) and cwd or root
+end
+
 local M = {}
 
 local filetypes = { 'php', 'blade', 'php_only' }
 
 local settings = {
-  filetypes = { 'php', 'blade', 'php_only' },
   files = {
-    associations = { '*.php', '*.blade.php' },
-    maxSize = 5000000,
+    maxSize = 10000000, -- 10MB
+  },
+  stubs = {"bcmath", "bz2", "Core", "curl", "date", "dom", "fileinfo", "filter", "gd", "gettext", "hash", "iconv", "imap", "intl", "json", "libxml", "mbstring", "mcrypt", "mysql", "mysqli", "password", "pcntl", "pcre", "PDO", "pdo_mysql", "Phar", "readline", "regex", "session", "SimpleXML", "sockets", "sodium", "standard", "superglobals", "tokenizer", "xml", "xdebug", "xmlreader", "xmlwriter", "yaml", "zip", "zlib", "wordpress", "wordpress-stubs", "woocommerce-stubs", "acf-pro-stubs", "wordpress-globals", "wp-cli-stubs", "genesis-stubs", "polylang-stubs"},
+  environment = {
+    includePaths = {
+      root_dir() .. '/vendor/php-stubs/',
+    },
   },
 }
 
