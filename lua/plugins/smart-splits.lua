8971379b2a2f96614c8fdcf5fd1a2e33934190c3
Eduardo Battisti <eduardobattisti10@gmail.com>
2025-06-15 17:37:19 -0300

    refactor: remap keymap according to lazy pattern

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,13 +1,10 @@
 return {
   'mrjones2014/smart-splits.nvim',
   opts = {},
-  config = function(_, opts)
-    local smart_splits = require 'smart-splits'
-    vim.keymap.set('n', '<A-h>', smart_splits.resize_left, { desc = 'Resize buffer left' })
-    vim.keymap.set('n', '<A-j>', smart_splits.resize_down, { desc = 'Resize buffer down' })
-    vim.keymap.set('n', '<A-k>', smart_splits.resize_up, { desc = 'Resize buffer up' })
-    vim.keymap.set('n', '<A-l>', smart_splits.resize_right, { desc = 'Resize buffer right' })
-
-    return opts
-  end,
+  keys = {
+    { '<A-h>', function() require('smart-splits').resize_left() end, desc = 'Resize buffer left', mode = 'n' },
+    { '<A-j>', function() require('smart-splits').resize_down() end, desc = 'Resize buffer down', mode = 'n' },
+    { '<A-k>', function() require('smart-splits').resize_up() end, desc = 'Resize buffer up', mode = 'n' },
+    { '<A-l>', function() require('smart-splits').resize_right() end, desc = 'Resize buffer right', mode = 'n' },
+  },
 }
